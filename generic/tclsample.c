f1b68defcbd83d91d661ee61b356803ee7cf7889
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-01-11 12:23:15 +0000

    Add "sample::build-info" command, for Tcl 8.7+

--- generic/tclsample.c
+++ generic/tclsample.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "sample.h"
+#include "sampleUuid.h"
 
 #define TCL_READ_CHUNK_SIZE 4096
 
@@ -347,8 +348,11 @@ wrongArgs:
 extern "C" {
 #endif  /* __cplusplus */
 DLLEXPORT int
-Sample_Init(Tcl_Interp *interp)
+Sample_Init(
+    Tcl_Interp* interp)		/* Tcl interpreter */
 {
+    Tcl_CmdInfo info;
+
     /*
      * This may work with 8.0, but we are using strictly stubs here,
      * which requires 8.1.
@@ -356,7 +360,67 @@ Sample_Init(Tcl_Interp *interp)
     if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
 	return TCL_ERROR;
     }
-    if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
+
+    if (Tcl_GetCommandInfo(interp, "::tcl::build-info", &info)) {
+	Tcl_CreateObjCommand(interp, "::sample::build-info",
+		info.objProc, (void *)(
+		    PACKAGE_VERSION "+" STRINGIFY(SAMPLE_VERSION_UUID)
+#if defined(__clang__) && defined(__clang_major__)
+			    ".clang-" STRINGIFY(__clang_major__)
+#if __clang_minor__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__clang_minor__)
+#endif
+#if defined(__cplusplus) && !defined(__OBJC__)
+			    ".cplusplus"
+#endif
+#ifndef NDEBUG
+			    ".debug"
+#endif
+#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__)
+			    ".gcc-" STRINGIFY(__GNUC__)
+#if __GNUC_MINOR__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__GNUC_MINOR__)
+#endif
+#ifdef __INTEL_COMPILER
+			    ".icc-" STRINGIFY(__INTEL_COMPILER)
+#endif
+#ifdef TCL_MEM_DEBUG
+			    ".memdebug"
+#endif
+#if defined(_MSC_VER)
+			    ".msvc-" STRINGIFY(_MSC_VER)
+#endif
+#ifdef USE_NMAKE
+			    ".nmake"
+#endif
+#ifndef TCL_CFG_OPTIMIZED
+			    ".no-optimize"
+#endif
+#ifdef __OBJC__
+			    ".objective-c"
+#if defined(__cplusplus)
+			    "plusplus"
+#endif
+#endif
+#ifdef TCL_CFG_PROFILED
+			    ".profile"
+#endif
+#ifdef PURIFY
+			    ".purify"
+#endif
+#ifdef STATIC_BUILD
+			    ".static"
+#endif
+		), NULL);
+    }
+
+    /* Provide the current package */
+
+    if (Tcl_PkgProvideEx(interp, PACKAGE_NAME, PACKAGE_VERSION, NULL) != TCL_OK) {
 	return TCL_ERROR;
     }
     Tcl_CreateObjCommand(interp, "sha1", (Tcl_ObjCmdProc *) Sha1_Cmd,
